942e415b20f0e680efe9ed7cb912223601ef6786
Puigui101 <Magikerman7@gmail.com>
2026-06-03 15:57:01 -0300

    Fuel tank

--- Assets/Scripts/FuelRecharge.cs
+++ Assets/Scripts/FuelRecharge.cs
@@ -2,32 +2,14 @@ using UnityEngine;
 
 public class FuelRecharge : MonoBehaviour
 {
-    [SerializeField] private float rotSpeed;
-    [SerializeField] private float verticalSpeed;
-    private Vector3 originalPos;
-
-    private float sineValue;
-
-    private void Awake()
-    {
-        originalPos = transform.position;
-    }
-
-    private void FixedUpdate()
-    {
-        sineValue = Mathf.Sin(Time.time) * verticalSpeed;
-
-        transform.Rotate(new Vector3(0,Time.fixedDeltaTime * rotSpeed, 0));
-        transform.position = new Vector3(originalPos.x, originalPos.y + sineValue, originalPos.z);
-    }
-
+    [SerializeField] private float refill;
     private void OnTriggerEnter(Collider other)
     {
         FuelTank fuel = other.GetComponent<FuelTank>();
 
         if (fuel != null)
         {
-            fuel.Refill(30f);
+            fuel.Refill(refill);
             Destroy(gameObject);
         }
     }
