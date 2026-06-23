ac4f2e95af5d7091ef77bb4ae3c9965113ba3247
Magikerman <Magikerman7@gmail.com>
2026-06-23 15:26:27 -0300

    Fps counter + android

---
+++ Assets/Scripts/FPScounter.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class FPScounter : MonoBehaviour
+{
+    private TextMeshProUGUI fpsText;
+    private float pollingTime = 0.5f;
+    private float time;
+    private int frameCount;
+
+    private void Awake()
+    {
+        fpsText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        time += Time.unscaledDeltaTime;
+        frameCount++;
+
+        if (time >= pollingTime)
+        {
+            int frameRate = Mathf.RoundToInt(frameCount / time);
+            fpsText.text = "FPS: " + frameRate;
+
+            time -= pollingTime;
+            frameCount = 0;
+        }
+    }
+}
