ab7611a648b77d78d364ad5eac92f32f8f2e6cab
Magikerman <Magikerman7@gmail.com>
2026-06-23 17:20:00 -0300

    Apk + controles

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -9,7 +9,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
 
     private float speed;
-    private float rotation;
+    //private float rotation;
+    private Vector3 rotation;
 
     public float Speed => speed;
 
@@ -25,6 +26,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private TextMeshProUGUI scoreText;
 
+    [SerializeField] private Joystick joystick;
+
+    [SerializeField] private Vector3 axisFix;
+    [SerializeField] private Vector3 axisFixSecond;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,23 +42,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed = playerInput.actions["Drive"].ReadValue<Vector2>().y;
-        rotation = playerInput.actions["Drive"].ReadValue<Vector2>().x;
+        //speed = playerInput.actions["Drive"].ReadValue<Vector2>().y;
+        //rotation = playerInput.actions["Drive"].ReadValue<Vector2>().x;
 
-        if (speed < 0) rotation *= -1;
-
-        timeSurvived += Time.deltaTime;
-        realTimeScoreText.text = (float)((int)(timeSurvived * 10)) /10+ "s";
+        //if (speed < 0) rotation *= -1;
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(transform.forward * speed * speedMult * Time.fixedDeltaTime, ForceMode.Impulse);
+        /*rb.AddForce(transform.forward * speed * speedMult * Time.fixedDeltaTime, ForceMode.Impulse);
 
         float speedRotModifier = Mathf.Clamp(rb.linearVelocity.magnitude / 10, 0, 10);
         Vector3 rot = transform.right * rotation * rotationMult * speedRotModifier;
 
-        transform.forward = Vector3.Lerp(transform.forward, rot, Time.fixedDeltaTime);
+        transform.forward = Vector3.Lerp(transform.forward, rot, Time.fixedDeltaTime);*/
+
+        rotation = axisFix * joystick.Horizontal + axisFixSecond * joystick.Vertical;
+        //rotation = Vector3.forward * joystick.Horizontal + -Vector3.right * joystick.Vertical + axisFix;
+
+        rotation.y = transform.forward.y;
+
+        Vector3 dir = transform.forward;
+        dir.y = 0;
+
+        if (rotation.x != 0 && rotation.z != 0)
+        {
+            transform.forward = Vector3.Lerp(transform.forward, rotation, rotationMult * Time.fixedDeltaTime);
+            rb.AddForce(dir * speedMult * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+
+        timeSurvived += Time.fixedDeltaTime;
+        realTimeScoreText.text = (float)((int)(timeSurvived * 10)) / 10 + "s";
     }
 
     public void Die()
