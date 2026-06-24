6e8ee2583dcb6b703ea0df622f0ece689ca411aa
Magikerman <Magikerman7@gmail.com>
2026-06-24 05:19:28 -0300

    Entrega miercoles

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public static LoadScene sceneLoader;
+
+    [SerializeField] private Image progress;
+    [SerializeField] private GameObject loadPanel;
+
+    private void Awake()
+    {
+        sceneLoader = this;
+    }
+
+    public void SceneLoad(int sceneIndex)
+    {
+        loadPanel.SetActive(true);
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncOperation.isDone)
+        {
+            Debug.Log(asyncOperation.progress);
+            progress.fillAmount = asyncOperation.progress / 0.9f;
+            yield return null;
+        }
+    }
+}
